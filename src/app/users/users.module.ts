cad7f130f88917e879fb5675604b69058b522400
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2024-09-10 11:31:31 -0600

    add: inicio de la aplicacion

---
+++ src/app/users/users.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersDashboardComponent } from './users-dashboard/users-dashboard.component';
+import { UserFormComponent } from './user-form/user-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    UsersDashboardComponent,
+    UserFormComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UsersModule { }
